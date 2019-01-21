d4b02f78328fbd234c8ea753bc856900c6e1606b
halit <you@example.com>
2019-01-21 23:55:29 +0300

    Using webflux

--- src/main/java/demo/piano/StackExchangeSearch/Config.java
+++ src/main/java/demo/piano/StackExchangeSearch/Config.java
@@ -1,7 +1,5 @@
 package demo.piano.StackExchangeSearch;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
--- src/main/java/demo/piano/StackExchangeSearch/HttpController.java
+++ src/main/java/demo/piano/StackExchangeSearch/HttpController.java
@@ -1,18 +1,17 @@
 package demo.piano.StackExchangeSearch;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import reactor.core.publisher.Mono;
 
-
-@RestController
+@Controller
 public class HttpController {
 
-    @RequestMapping(value = "/search", method = RequestMethod.GET, produces="application/json")
-    public String search(@RequestParam(value="searchString", defaultValue="") String searchString, StackExchangeApi stackExchangeApi) {
-        return stackExchangeApi.search(searchString);
+    @GetMapping("/search")
+    @ResponseBody
+    public Mono<String> search(@RequestParam("searchString") String searchString, StackExchangeApi stackExchangeApi) {
+        return Mono.just(stackExchangeApi.search(searchString)) ;
     }
 }
